25139863dc2361a1d0d376a166661f3129cb401d
cauafj <cauafjz@gmail.com>
2023-04-18 15:03:50 -0300

    projeto já com todas as funções funcionando devidamente. Agora é possível gerenciar os produtos através do frontend

--- src/pages/components/changingMenu.jsx
+++ src/pages/components/changingMenu.jsx
@@ -1,19 +1,26 @@
 import api from "../../services/api.js"
-import { useState, useEffect } from "react"
+import { useState } from "react"
+import "../../styles/changingMenu.css"
 
 export default function ChangingMenu(props) {
-    const opt = props.opt
+    const { opt, prods } = props
     const [editedProd, setEditedProd] = useState()
-    const [prods, setProds] = useState([])
+    const [produc, setProduc] = useState({
+        id: 0,
+        name: "",
+        price: 0,
+        type: "xis"
+    })
 
-    useEffect(() => {
-        api.get("products").then((res) =>{
-            setProds(res.data)
+    const sendNewProd = () => {
+        setProduc(produc)
+        api.post("addProd", produc)
+        setProduc({
+            id: 0,
+            name: "",
+            price: 0,
+            type: "xis"
         })
-    }, [])
-
-    const sendNewProd = (product) => {
-        api.post("addProd", product)
         props.render(0)
     }
 
@@ -24,26 +31,33 @@ export default function ChangingMenu(props) {
 
     const editProd = (product) => {
         api.post("editProd", product)
+        setEditedProd()
         props.render(0)
     }
 
     const prodForm = (product) => {
+        let productTypes = ["xis", "hamburguer", "cachorroquente", "porcao", "bebida", "combo"]
+        productTypes = productTypes.filter(e => e !== product.type)
+        const selectType = (
+            <select defaultValue={product.type} onChange={(e) => product.type = e.target.value}>
+                <option value={product.type}>{product.type[0].toUpperCase() + product.type.substring(1)}</option>
+                {productTypes.map(e => {
+                    return (
+                        <option value={e}>{e[0].toUpperCase() + e.substring(1)}</option>
+                    )
+                })}
+            </select>
+        )
+
         return (
-            <div className="changeContainer">
+            <div className="formContainer">
                     <label>
                        Nome: <input type="text" defaultValue={product.name} onChange={(e) => product.name = e.target.value}/>
                     </label>
                     <label>
                        Preço: <input type="text" defaultValue={product.price} onChange={(e) => product.price = e.target.value}/>
                     </label>
-                    <select defaultValue={product.type} onChange={(e) => product.type = e.target.value}>
-                        <option value="xis">Xis</option>
-                        <option value="hamburguer">Hamburguer</option>
-                        <option value="cachorroquente">Cachorro quente</option>
-                        <option value="porcao">Porção</option>
-                        <option value="combo">Combo</option>
-                        <option value="bebida">Bebida</option>
-                    </select>
+                    {selectType}
                     <button onClick={() => editProd(product)}>Enviar</button>
             </div>
         )
@@ -51,32 +65,28 @@ export default function ChangingMenu(props) {
 
     switch(opt){
         case 0:
-            return <div className="changeContainer"></div>
+            console.log("aqui")
+            break
 
         case 1: //editar
             return (
-                <div className="changeContainer">
-                {prods.map(e => {
-                    return (
-                        <button onClick={() => setEditedProd(prodForm(e))}>{e.name}</button>
-                    )
-                })}
+                <div className="editContainer">
+                <div className="changeContainer ">
+                    {prods.map(e => {
+                        return (
+                            <button onClick={() => setEditedProd(prodForm(e))}>{e.name}</button>
+                        )
+                    })}
+                </div>
         
                 {editedProd}
 
                 </div>
             )
 
-
         case 2: //adicionar
-            const produc = {
-                name: "",
-                price: 0,
-                type: ""
-            }
-
             return (
-                <div className="changeContainer">
+                <div className="formContainer">
                     <label>
                        Nome: <input type="text" onChange={(e) => produc.name = e.target.value}/>
                     </label>
@@ -91,11 +101,11 @@ export default function ChangingMenu(props) {
                         <option value="combo">Combo</option>
                         <option value="bebida">Bebida</option>
                     </select>
-                    <button onClick={() => sendNewProd(produc)}>Enviar</button>
+                    <button onClick={() => sendNewProd()}>Enviar</button>
                 </div>
             )
 
-        case 3: //remover
+        case 3: //remover   
             return (
                 <div className="changeContainer">
                     {prods.map(e => {
@@ -105,6 +115,8 @@ export default function ChangingMenu(props) {
                     })}
                 </div>
             )
+        default:
+            console.log("Não foi enviado o state que determina qual deverá ser o menu de escolha para edição de produto.")
     }
 
 }
--- src/pages/components/editProds.jsx
+++ src/pages/components/editProds.jsx
@@ -1,21 +1,28 @@
-import "../../styles/cadProds.css"
+import "../../styles/editProds.css"
 import { useState } from "react"
-import ChanginMenu from "./changinMenu.jsx"
-import api from "../../services/api"
+import ChanginMenu from "./changingMenu.jsx"
+import api from "../../services/api.js"
 
-export default function CadProds(props) {
+
+export default function CadProds() {
     const [changingMenu, setChangingMenu] = useState(0)
-
+    const [prods, setProds] = useState([])
+    const att = (state) => {
+        api.get("products").then(res => {
+            setProds(res.data)
+        })
+        setChangingMenu(state)
+    }
 
     return (
         <div className="cadContainer">
             <h2>O que você deseja fazer?</h2>
             <div className="choiceOpt">
-                <button onClick={() => setChangingMenu(1)}>Editar produto</button>
-                <button onClick={() => setChangingMenu(2)}>Adicionar produto </button>
-                <button onClick={() => setChangingMenu(3)}>Remover produto</button>
+                <button onClick={() => att(1)}>Editar produto</button>
+                <button onClick={() => att(2)}>Adicionar produto </button>
+                <button onClick={() => att(3)}>Remover produto</button>
             </div>
-            <ChanginMenu opt={changingMenu} render={setChangingMenu}></ChanginMenu>
+            <ChanginMenu opt={changingMenu} render={setChangingMenu} prods={prods}></ChanginMenu>
         </div>
     )
 }
